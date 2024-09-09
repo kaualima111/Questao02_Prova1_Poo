2f9feeff6d8c8e2249e35cd76f210b64b9faaeff
kaualima111 <158177741+kaualima111@users.noreply.github.com>
2024-09-09 16:34:42 -0300

    Add files via upload

---
+++ FormasAlgebricas/FormasAlgebricas/NumComplexo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumComplexoApp
+{
+    class NumComplexo
+    {
+        public double Real { get; private set; }
+        public double Imaginario { get; private set; }
+
+        public NumComplexo(double real, double imaginario)
+        {
+            Real = real;
+            Imaginario = imaginario;
+        }
+
+        public NumComplexo Soma(NumComplexo outro)
+        {
+            return new NumComplexo(this.Real + outro.Real, this.Imaginario + outro.Imaginario);
+        }
+
+        public NumComplexo Vezes(NumComplexo outro)
+        {
+            double novoReal = (this.Real * outro.Real) - (this.Imaginario * outro.Imaginario);
+            double novoImaginario = (this.Real * outro.Imaginario) + (this.Imaginario * outro.Real);
+            return new NumComplexo(novoReal, novoImaginario);
+        }
+
+        public double Modulo()
+        {
+            return Math.Sqrt(Real * Real + Imaginario * Imaginario);
+        }
+
+        public double Argumento()
+        {
+            return Math.Atan2(Imaginario, Real);
+        }
+
+        public void ImprimeFormaPolar()
+        {
+            double modulo = Modulo();
+            double argumento = Argumento();
+            Console.WriteLine($"Forma Polar: {modulo} (cos({argumento}) + i*sin({argumento}))");
+        }
+    }
+}
---
+++ FormasAlgebricas/FormasAlgebricas/Program.cs
@@ -0,0 +1,37 @@
+﻿using NumComplexoApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Kauã Cordeiro Durães de Lima - 12311ECP017
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a parte real (x) do primeiro número complexo: ");
+        double real1 = double.Parse(Console.ReadLine());
+        Console.Write("Digite a parte imaginária (y) do primeiro número complexo: ");
+        double imaginario1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a parte real (x) do segundo número complexo: ");
+        double real2 = double.Parse(Console.ReadLine());
+        Console.Write("Digite a parte imaginária (y) do segundo número complexo: ");
+        double imaginario2 = double.Parse(Console.ReadLine());
+
+        NumComplexo z1 = new NumComplexo(real1, imaginario1);
+        NumComplexo z2 = new NumComplexo(real2, imaginario2);
+
+        NumComplexo soma = z1.Soma(z2);
+        Console.WriteLine("Soma:");
+        soma.ImprimeFormaPolar();
+
+        NumComplexo produto = z1.Vezes(z2);
+        Console.WriteLine("Produto:");
+        produto.ImprimeFormaPolar();
+
+        Console.ReadLine();
+    }
+}
